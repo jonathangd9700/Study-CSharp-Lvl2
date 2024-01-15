d95324de4c8e666792c8601292e28abeec00f5b3
Jonathan Dominguez <jonathan-g-dominguez@outlook.es>
2024-01-15 20:25:37 -0300

    Agregar archivos de proyecto.

---
+++ Herencia/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public int Edad { get; set; }
+
+        public string Greet() 
+        {
+            return $"Hola mi nombre es {Nombre}";
+        }
+    }
+}
---
+++ Herencia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona();
+
+            p1.Nombre = "Abril";
+
+            Developer d1 = new Developer();
+
+            d1.Nombre = "Jon";
+
+            Console.WriteLine(p1.Greet());
+
+            Console.WriteLine(d1.Greet());
+
+            Console.ReadKey();
+        }
+    }
+}
